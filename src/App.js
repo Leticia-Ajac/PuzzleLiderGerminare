390ab201b9850c2e32d650f00f29d288d314c9fb
ajac <leticiajac2018@gmail.com>
2022-06-20 15:11:57 -0300

    introLevel1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css'
 import LevelUmPage from './Pages/Level1';
 import BeginningPage from './Pages/Beginning'
 import FirstVideoPage from './Pages/FirstVideo'
+import IntroLevel1Page from './Pages/IntroLevel1'
 
 function App() {
   return (
@@ -16,8 +17,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<BeginningPage />} />
-          <Route path='/level1' element={<LevelUmPage />}/>
           <Route path='/firstVideo' element={<FirstVideoPage />}/>
+          <Route path='/IntroLevel1Page' element={<IntroLevel1Page />}/>
+          <Route path='/level1' element={<LevelUmPage />}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Pages/IntroLevel1/index.jsx
@@ -0,0 +1,14 @@
+import Borda from "../../Components/Border"
+import imagem from '../../Assets/DRADIN.png'
+import './style.css'
+
+
+export default function IntroLevel1Page(){
+
+    return(
+        <Borda corBorda="#fff" >
+            <img src={imagem} alt="" className="girar" style={{width:'150px'}} />
+            <p style={{fontSize:'32px', margin:'50px 0 -20px'}} >LEVEL 1</p>
+        </Borda>
+    )
+}
