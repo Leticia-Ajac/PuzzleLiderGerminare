d3dd0df7a77d62e83f5c8071772eed58b5a41ecd
ajac <leticiajac2018@gmail.com>
2022-06-22 20:18:36 -0300

    intro route

--- src/Pages/Intros/index.jsx
+++ src/Pages/Intros/index.jsx
@@ -2,13 +2,32 @@ import Borda from "../../Components/Border"
 import imagem from '../../Assets/DRADIN.png'
 import './style.css'
 import { Press } from "../../Components/Press/style";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useCountdown, ExpiredNotice, ShowCounter } from '../../Components/TimerIntro1/index';
 
 export function IntroLevel1Page(){
+    const CountdownTimer = ({ targetDate }) => {
+        const [days, hours, minutes, seconds] = useCountdown(targetDate);
+        
+            if (days + hours + minutes + seconds <= 0) {
+            return <ExpiredNotice />;
+            } else {
+            return (
+                <ShowCounter
+                minutes={minutes}
+                seconds={seconds}
+                />
+            );
+            }
+        };
+
+    const agora = new Date().getTime();
+    const dateTimeAfterThreeDays = agora + 2000;
     return(
         <Borda corBorda="#fff" justifyContent="center" >
             <img src={imagem} alt="" className="girar" style={{width:'150px'}} />
-            <p style={{fontSize:'32px', margin:'50px 0 -20px'}} >LEVEL 1</p>
+            <p style={{fontSize:'32px', margin:'50px 0 20px'}} >LEVEL 1</p>
+            <CountdownTimer className targetDate={dateTimeAfterThreeDays}  />
         </Borda>
     )
 }
