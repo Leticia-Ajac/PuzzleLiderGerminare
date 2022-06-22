aad0f1c5e8d32f8fe0545e9c29978788120fe963
Gabriel-Liondas <liondas.gabriel@gmail.com>
2022-06-22 13:55:53 -0300

    lv3 error

--- src/Pages/Level3/index.jsx
+++ src/Pages/Level3/index.jsx
@@ -7,6 +7,8 @@ import Borda from "../../Components/Border/index";
 
 
 import './style.css'
+import { getElementError } from "@testing-library/react";
+import { ContentPasteSearchOutlined } from "@mui/icons-material";
 
 export default function LevelUmPage() {
     var selecionados = []
@@ -27,20 +29,45 @@ export default function LevelUmPage() {
         };
         
     function selecionar(e) {
-        const cor = e.target.style.color
+        const idEl = e.target.id
 
-        if (cor == 'rgb(255, 255, 255)' ) {
-            e.target.style.color = '#930000'
-            e.target.style.background = 'none'
-            e.target.style.border = '1px solid #930000'
-            selecionados.splice(selecionados.indexOf(e.target.innerText), 1)
+        if (selecionados.includes(idEl)) {
+            e.target.classList.remove('selecionado');
+            selecionados.splice(selecionados.indexOf(idEl), 1)
         } else {
-            e.target.style.color = '#fff'
-            e.target.style.background = '#ffffff34'
-            e.target.style.border = '2px solid #ffffff'
-            selecionados.push(e.target.innerText)
+            e.target.classList.add('selecionado')
+            selecionados.push(idEl)
         }
         console.log(selecionados)
+    }
+
+    function onSubmit(e) {
+        // document.getElementsByClassName("selecionado").forEach(function(element, i) {
+        //     console.log(element, i);
+        // })
+
+        const selecionadosClasse = Array.from(
+            document.getElementsByClassName("selecionado")
+        );
+        selecionadosClasse.forEach(element => {
+            element.classList.add('errado')
+            setTimeout(() => {
+                selecionadosClasse.forEach(element => {
+                    element.classList.remove('selecionado')
+                    element.classList.remove('errado')
+                })
+                selecionados=[]
+            }, 3000)
+        })
+
+        setTimeout(() => {
+            selecionadosClasse.forEach(element => {
+                element.classList.remove('selecionado')
+                element.classList.remove('errado')
+            })
+            selecionados=[]
+        }, 3000)
+        
     }
 
     const agora = new Date().getTime();
@@ -55,24 +82,24 @@ export default function LevelUmPage() {
 
             <div className="esquerdaContainer">
                 <div class="grid-container">
-                    <button onClick={selecionar} class={`grid-item`}>1</button>
-                    <button onClick={selecionar} class={`grid-item`}>2</button>
-                    <button onClick={selecionar} class={`grid-item`}>3</button>
-                    <button onClick={selecionar} class={`grid-item`}>4</button>
-                    <button onClick={selecionar} class={`grid-item`}>5</button>
-                    <button onClick={selecionar} class={`grid-item`}>6</button>
-                    <button onClick={selecionar} class={`grid-item`}>7</button>
-                    <button onClick={selecionar} class={`grid-item`}>8</button>
-                    <button onClick={selecionar} class={`grid-item`}>9</button>
-                    <button onClick={selecionar} class={`grid-item`}>10</button>
-                    <button onClick={selecionar} class={`grid-item`}>11</button>
-                    <button onClick={selecionar} class={`grid-item`}>12</button>
+                    <button onClick={selecionar} id='1' class={`grid-item`}>Liderança heróica</button>
+                    <button onClick={selecionar} id='2' class={`grid-item`}>Desafiar o estabelecido</button>
+                    <button onClick={selecionar} id='3' class={`grid-item`}>Liderança por exemplo</button>
+                    <button onClick={selecionar} id='4' class={`grid-item`}>Entregar de resultados</button>
+                    <button onClick={selecionar} id='5' class={`grid-item`}>Liderança diplomática</button>
+                    <button onClick={selecionar} id='6' class={`grid-item`}>Direcionar</button>
+                    <button onClick={selecionar} id='7' class={`grid-item`}>Centralizar</button>
+                    <button onClick={selecionar} id='8' class={`grid-item`}>Visão compartilhada</button>
+                    <button onClick={selecionar} id='9' class={`grid-item`}>Liderança agitada</button>
+                    <button onClick={selecionar} id='10' class={`grid-item`}>Inspirar</button>
+                    <button onClick={selecionar} id='11' class={`grid-item`}>Controlar as pessoas</button>
+                    <button onClick={selecionar} id='12' class={`grid-item`}>Aberto à inovação</button>
                 </div>
             </div>
 
                 <div className="direitaContainer" style={{width:'400px', border:'1px solid red', display:'flex',flexDirection:'column', alignItems:'center', justifyContent:'space-around'}} >
                     <CountdownTimer className targetDate={dateTimeAfterThreeDays}  />
-                    <button className="btnSubmit"> {'>'} </button>
+                    <button className="btnSubmit" onClick={onSubmit}> {'>'} </button>
                 </div>
 
 
