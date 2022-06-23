345229933368b48a0896c0cac53b7194a60a7f93
ajac <leticiajac2018@gmail.com>
2022-06-23 10:04:39 -0300

    autoplay firstVideo

--- src/Pages/FirstVideo/index.jsx
+++ src/Pages/FirstVideo/index.jsx
@@ -1,14 +1,12 @@
 import Borda from "../../Components/Border"
 import gif from '../../Assets/questionMark.gif'
-import som from '../../Assets/fistVideo.mp3'
+import som from '../../Assets/firstVideo.mp3'
 
 import * as React from "react";
 import { Component } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import ReactAudioPlayer from 'react-audio-player';
 
-class AutoPlay extends Component {
-    
-}
 
 export default function FirstVideoPage() {
     const navegate = useNavigate()
@@ -16,14 +14,16 @@ export default function FirstVideoPage() {
         function(){
             document.getElementById('nav').style.display = 'flex'
             return navegate
-        }, 52000
+        }, 1000
     );
 
     return (
         <Borda corBorda="#fff" justifyContent="center" >
             <img src={gif} alt="gif" style={{border:'1px solid #fff'}} />
-            <p></p>
-            <Link style={{display:'none'}} id="nav" className="i1_input" onClick={nav} to="/Level1" > {'>'} </Link>
+            <div style={{display:'none'}}>
+            <ReactAudioPlayer src={som} autoPlay controls />
+            </div>
+            <Link style={{display:'none'}} id="nav" className="i1_input" onClick={nav} to="/Rules" > {'>'} </Link>
         </Borda>
     )
 }
