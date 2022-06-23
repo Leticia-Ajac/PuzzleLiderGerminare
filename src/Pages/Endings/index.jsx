b76401521f8ca12259edc2ca32354175f4525452
ajac <leticiajac2018@gmail.com>
2022-06-23 00:30:16 -0300

    endings Link navegate

--- src/Pages/Endings/index.jsx
+++ src/Pages/Endings/index.jsx
@@ -1,16 +1,17 @@
 import Borda from '../../Components/Border/index'
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Press } from "../../Components/Press/style";
 import END1 from '../../Assets/END1.png'
 import END2 from '../../Assets/END2.png'
 import END3 from '../../Assets/END3.png'
 
 export function EndingLevel1Page(){
+    const navegate = useNavigate()
     return(
         <Borda corBorda="#fff" justifyContent="center" >
             <p style={{fontSize:'32px', margin:'0 0 50px'}} >PARABÉNS</p>
             <img src={END1} alt="" style={{width:'150px'}} />
-            <p style={{fontSize:'32px', margin:'50px 0 -20px'}} >MÃOS PARA CIMA!!!</p>
+            <Link onClick={navegate} to='/IntroLevel2'style={{fontSize:'32px', margin:'50px 0 -20px', textDecoration:'none', color:'white'}} >MÃOS PARA CIMA!!!</Link>
         </Borda>
     )
 }
@@ -21,7 +22,7 @@ export function EndingLevel2Page(){
         <Borda corBorda="#fff" justifyContent="center" >
             <p style={{fontSize:'32px', margin:'0 0 50px'}} >PARABÉNS</p>
             <img src={END2} alt="" style={{width:'150px'}} />
-            <p style={{fontSize:'32px', margin:'50px 0 -20px'}} >MÃOS PARA CIMA!!!</p>
+            <Link onClick={navegate} to='/IntroLevel3'style={{fontSize:'32px', margin:'50px 0 -20px', textDecoration:'none', color:'white'}} >MÃOS PARA CIMA!!!</Link>
         </Borda>
     )
 }
@@ -32,7 +33,7 @@ export function EndingLevel3Page(){
         <Borda corBorda="#fff" justifyContent="center" >
             <p style={{fontSize:'32px', margin:'0 0 50px'}} >PARABÉNS</p>
             <img src={END3} alt="" style={{width:'150px'}} />
-            <p style={{fontSize:'32px', margin:'50px 0 -20px'}} >MÃOS PARA CIMA!!!</p>
+            <Link onClick={navegate} to='/IntroLevel2'style={{fontSize:'32px', margin:'50px 0 -20px', textDecoration:'none', color:'white'}} >MÃOS PARA CIMA!!!</Link>
         </Borda>
     )
 }
