c8740e7d2d9155139e075a25a0283df314317f37
ajac <leticiajac2018@gmail.com>
2022-06-22 21:26:30 -0300

    endings

--- src/Pages/Endings/index.jsx
+++ src/Pages/Endings/index.jsx
@@ -1,14 +1,15 @@
 import Borda from '../../Components/Border/index'
 import { useNavigate } from "react-router-dom";
-import imagem from '../../Assets/DRADIN.png'
-import './style.css'
 import { Press } from "../../Components/Press/style";
+import END1 from '../../Assets/END1.png'
+import END2 from '../../Assets/END2.png'
+import END3 from '../../Assets/END3.png'
 
 export function EndingLevel1Page(){
     return(
         <Borda corBorda="#fff" justifyContent="center" >
             <p style={{fontSize:'32px', margin:'0 0 50px'}} >PARABÉNS</p>
-            <img src={imagem} alt="" className="girar" style={{width:'150px'}} />
+            <img src={END1} alt="" style={{width:'150px'}} />
             <p style={{fontSize:'32px', margin:'50px 0 -20px'}} >MÃOS PARA CIMA!!!</p>
         </Borda>
     )
@@ -19,7 +20,7 @@ export function EndingLevel2Page(){
     return(
         <Borda corBorda="#fff" justifyContent="center" >
             <p style={{fontSize:'32px', margin:'0 0 50px'}} >PARABÉNS</p>
-            <img src={imagem} alt="" className="girar" style={{width:'150px'}} />
+            <img src={END2} alt="" style={{width:'150px'}} />
             <p style={{fontSize:'32px', margin:'50px 0 -20px'}} >MÃOS PARA CIMA!!!</p>
         </Borda>
     )
@@ -30,7 +31,7 @@ export function EndingLevel3Page(){
     return(
         <Borda corBorda="#fff" justifyContent="center" >
             <p style={{fontSize:'32px', margin:'0 0 50px'}} >PARABÉNS</p>
-            <img src={imagem} alt="" className="girar" style={{width:'150px'}} />
+            <img src={END3} alt="" style={{width:'150px'}} />
             <p style={{fontSize:'32px', margin:'50px 0 -20px'}} >MÃOS PARA CIMA!!!</p>
         </Borda>
     )
