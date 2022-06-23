f0809d5ca2b5647aa1afc3619c14cb3e7ce0a138
ajac <leticiajac2018@gmail.com>
2022-06-23 14:03:58 -0300

    images level2

--- src/Pages/Level2/index.jsx
+++ src/Pages/Level2/index.jsx
@@ -5,6 +5,18 @@ import bola from "../../Assets/DRADIN.png";
 import { useCountdown, ExpiredNotice, ShowCounter } from '../../Components/Timer2/index';
 import InputLevel2 from "../../Components/InputLevel2/index"
 import './style.css'
+import um from '../../Assets/E1.png'
+import dois from '../../Assets/X2.png'
+import tres from '../../Assets/P3.png'
+import quatro from '../../Assets/E4.png'
+import cinco from '../../Assets/R5.png'
+import seis from '../../Assets/I6.png'
+import sete from '../../Assets/E7.png'
+import oito from '../../Assets/N8.png'
+import nove from '../../Assets/C9.png'
+import dez from '../../Assets/I10.png'
+import onze from '../../Assets/A11.png'
+
 
 export default function LevelDoisPage(){
     const CountdownTimer = ({ targetDate }) => {
@@ -32,37 +44,37 @@ export default function LevelDoisPage(){
 
             <div style={{display:'flex', flexDirection:'row', widows:'90%', justifyContent:'space-around', margin:'20px auto'}} >
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={um} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={dois} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={tres} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={quatro} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={cinco} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={seis} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={sete} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={oito} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={nove} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={dez} alt="" className="uc" />
                 </Cubo>
                 <Cubo>
-                    <img src={bola} alt="" className="uc" />
+                    <img src={onze} alt="" className="uc" />
                 </Cubo>
             </div>
             <div>
