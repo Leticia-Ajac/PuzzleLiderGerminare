8ab4a6539405f819b68b8c84e5e117cb5fd33dff
ajac <leticiajac2018@gmail.com>
2022-06-23 11:06:01 -0300

    rules navegate

--- src/Pages/Rules/index.jsx
+++ src/Pages/Rules/index.jsx
@@ -1,10 +1,12 @@
 import Borda from "../../Components/Border"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function RulesPage(){
+    const nevagate = useNavigate()
     return(
         <Borda corBorda="#fff" color="#fff" justifyContent="center" >
-            <div style={{border:'1px solid #fff', width:'40%', height:'80%', fontSize:'30px', }} >
-                <p>Rules</p>
+            <div style={{border:'1px solid #fff', width:'40%', height:'80%', fontSize:'30px', padding:'40px 0 0' }} >
+                <Link onClick={nevagate} to='/SecretPassword' style={{textDecoration:'none', color:'white'}} >Rules</Link>
 
                 <div style={{ height:'60%', fontSize:'20px', listStyle:'none', display:'flex', flexDirection:'column', justifyContent:'space-evenly'}}>
                     <p>Proibido abrir o inspecionar</p>
