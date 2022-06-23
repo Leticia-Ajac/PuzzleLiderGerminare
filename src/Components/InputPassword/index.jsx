af45d83878aba0359e0e2c3beae00844c3fd3f8a
Gabriel-Liondas <liondas.gabriel@gmail.com>
2022-06-23 14:05:09 -0300

    final lv error

--- src/Components/InputPassword/index.jsx
+++ src/Components/InputPassword/index.jsx
@@ -1,61 +1,79 @@
 import * as React from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from 'react-hook-form'
-import { InputBox } from './style';
+import './style.css'
+
 
 export default function InputPassword() {
     const {register, getValues} = useForm()
     const navegate = useNavigate()
-    const [nana,setNana] = React.useState('/SecretPassword')
+    
+
+    function onSubmitLvFinal(event) {
+        console.log(event)
+        event.preventDefault()
+        const inputlvBrancoClasse = Array.from(
+            document.getElementsByClassName("inputBranco")
+        );
+        const mult = getValues(["teste1", "teste2", "teste3", "teste4", "teste5", "teste6", "teste7", "teste8", "teste9", "teste10", "teste11", "teste12"]);
+
+        if(mult.join('').toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "") == 'COLETIVIDADE'){
+            navegate("/SecondtVideo")
+        }else{
+            inputlvBrancoClasse.forEach(element => {
+                element.classList.add('erradoLvFinal')
+                setTimeout(() => {
+                    inputlvBrancoClasse.forEach(element => {
+                        element.classList.remove('erradoLvFinal')
+                    })
+                }, 500)
+                setTimeout(() => {
+                    inputlvBrancoClasse.forEach(element => {
+                        element.classList.add('erradoLvFinal')
+                    })
+                }, 1000)
+                setTimeout(() => {
+                    inputlvBrancoClasse.forEach(element => {
+                        element.classList.remove('erradoLvFinal')
+                    })
+                }, 1500)
+                setTimeout(() => {
+                    inputlvBrancoClasse.forEach(element => {
+                        element.classList.add('erradoLvFinal')
+                    })
+                }, 2000)
+                setTimeout(() => {
+                    inputlvBrancoClasse.forEach(element => {
+                        element.classList.remove('erradoLvFinal')
+                    })
+                }, 2500)
+        })
+        }
+    }
+
     return (
         <form style={{marginBottom:'30px'}} >
-            <InputBox maxLength={1} type="text" {...register("teste1")} autoFocus />
-            <InputBox maxLength={1} type="text" {...register("teste2")} />
-            <InputBox maxLength={1} type="text" {...register("teste3")} />
-            <InputBox maxLength={1} type="text" {...register("teste4")} />
-            <InputBox maxLength={1} type="text" {...register("teste5")} />
-            <InputBox maxLength={1} type="text" {...register("teste6")} />
-            <InputBox maxLength={1} type="text" {...register("teste7")} />
-            <InputBox maxLength={1} type="text" {...register("teste8")} />
-            <InputBox maxLength={1} type="text" {...register("teste9")} />
-            <InputBox maxLength={1} type="text" {...register("teste10")} />
-            <InputBox maxLength={1} type="text" {...register("teste11")} />
-            <InputBox maxLength={1} type="text" {...register("teste12")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste1")} autoFocus />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste2")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste3")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste4")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste5")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste6")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste7")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste8")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste9")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste10")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste11")} />
+            <input className='inputBranco' maxLength={1} type="text" {...register("teste12")} />
             <br></br>
-            <Link 
-            style={{margin:'50px 0 60px auto', fontSize:'20px', padding:'0 30px', background:'none',border:'1px solid #fff', color:'#fff', textDecoration:'none'}}
-
-            to={nana}
-
-            onClick={()=>{
-                const mult = getValues(["teste1", "teste2", "teste3", "teste4", "teste5", "teste6", "teste7", "teste8", "teste9", "teste10", "teste11", "teste12"]);
-                if(mult.join('').toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "") == 'COLETIVIDADE'){
-                    setNana('/SecondVideo')
-                    return navegate
-                }else{
-                    alert('nao foi dessa vez')
-                }
-            }}
-
-            > &gt; </Link>
+            <button 
+            style={{margin:'0 0 60px auto', fontSize:'20px', padding:'0 30px', background:'none',border:'1px solid #fff', color:'#fff', textDecoration:'none'}}
+            onClick={onSubmitLvFinal}
+            > &gt; </button>
             <br />
         </form>
     )
 }
 
-// export default function InputSeparado() {
-//     const [values, setValues] = React.useState(Array(11).fill(''))
-//     const { fields } = usePinInput({
-//         values,
-//         onChange: (values) => {setValues(values)},
-//     })
 
-//   return (
-//     <div className style={{margin:'0 0 30px'}}>
-//       {fields.map((propsField, index) => (
-//         <input key={index} {...propsField} style={{width:'57px', margin:' auto 5px', height: '57px', background:'none', border:'1px solid #5D1C7C', color:'#5D1C7C', textAlign: 'center', fontSize:'20px'}}  />
-//       ))}
-//     </div>
-//   )
-// }
 
